2ed8659eb60beec084eb5121875f79491bf2b952
clelio83 <clelio83@262607f0-e6ff-11dd-bce9-bf72c0239d3a>
2009-02-24 14:23:58 +0000

    versione 3. Accetta registrazione customer funzionante

--- SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/session/AccountBean.java
+++ SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/session/AccountBean.java
@@ -1,7 +1,10 @@
 package org.domain.SeamAmiciDelGas.session;
 
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
+
+import javax.persistence.EntityManager;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
@@ -9,11 +12,10 @@ import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.log.Log;
+import org.domain.SeamAmiciDelGas.crud.AccountHome;
 import org.domain.SeamAmiciDelGas.crud.AccountList;
 import org.domain.SeamAmiciDelGas.crud.RoleList;
 import org.domain.SeamAmiciDelGas.entity.Account;
-import org.domain.SeamAmiciDelGas.entity.Role;
-import org.domain.SeamAmiciDelGas.entity.Utente;
 
 @Name(value="accountBean")
 @Scope(ScopeType.SESSION)
@@ -28,9 +30,22 @@ public class AccountBean {
 	@In(value="roleList",create=true)
 	private RoleList roleList;
 	
+	@In(value="accountHome", create=true)
+	private AccountHome accountHome;
+	
 	private Account currentCustomer;
 	
 	private List<Account> richiesteCustomer;
+	
+	private boolean updeted = false;
+
+	public boolean isUpdeted() {
+		return updeted;
+	}
+
+	public void setUpdeted(boolean updeted) {
+		this.updeted = updeted;
+	}
 
 	//seleziono gli utenti customer che sono nello stato attivato 0 bloccato 0 eliminato 0
 	public List<Account> getRichiesteCustomer() {
@@ -39,16 +54,65 @@ public class AccountBean {
 		return richiesteCustomer;
 	}
 	
-	public void accettaRegistrazioneCustomer() {
-		log.info("**********************");
-		log.info("********** ha accettato la richiesta**********");
-		log.info("**********************");
+	public void reset() {
+		accountHome.setAccountUsername("emanuele");
+		Account a = accountHome.find();
+		if (a==null) {
+			log.info("********E' NULL**********");
+			this.setUpdeted(false);
+		}
+		log.info("*******"+a.getUsername()+"*********");
+		a.setAttivato(false);
+		a.setBloccato(false);
+		a.setElimato(false);
+		GregorianCalendar gc = new GregorianCalendar();
+		a.setDataAccettazione(gc.getTime());
+		accountHome.update();
+		this.setUpdeted(true);
 	}
 	
+	//modifico lo stato dell'account attivato 1 bloccato 0 eliminato 0
+	public void accettaRegistrazioneCustomer() {
+		try {
+			accountHome.setAccountUsername(currentCustomer.getUsername());
+			Account a = accountHome.find();
+			if (a==null) {
+				this.setUpdeted(false);
+				return;
+			}			
+			a.setAttivato(true);
+			a.setBloccato(false);
+			a.setElimato(false);
+			GregorianCalendar gc = new GregorianCalendar();
+			a.setDataAccettazione(gc.getTime());
+			accountHome.update();
+			this.setUpdeted(true);
+		}
+		catch (Exception e) {
+			log.info(e.toString());
+			this.setUpdeted(false);
+		}
+	}
+	
+	//modifico lo stato dell'account attivato 0 bloccato 0 eliminato 1
 	public void rifiutaRegistrazioneCustomer() {
-		log.info("**********************");
-		log.info("********** NON ha accettato la richiesta**********");
-		log.info("**********************");
+		try {
+			accountHome.setAccountUsername(currentCustomer.getUsername());
+			Account a = accountHome.find();
+			if (a==null) {
+				this.setUpdeted(false);
+				return;
+			}
+			a.setAttivato(false);
+			a.setBloccato(false);
+			a.setElimato(true);
+			accountHome.update();
+			this.setUpdeted(true);
+	}
+	catch (Exception e) {
+		log.info(e.toString());
+		this.setUpdeted(false);
+	}
 	}
 
 	public void setRichiesteCustomer(List<Account> richiesteCustomer) {
