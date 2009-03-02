86939a7bcd38459fd54bd4715cda54a3a6b85b28
clelio83 <clelio83@262607f0-e6ff-11dd-bce9-bf72c0239d3a>
2009-03-02 21:25:22 +0000

    prima versione di visualizzazione ordini

---
+++ trunk/SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/session/OrdineBean.java
@@ -0,0 +1,62 @@
+package org.domain.SeamAmiciDelGas.session;
+
+import java.util.List;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Credentials;
+import org.domain.SeamAmiciDelGas.crud.AccountList;
+import org.domain.SeamAmiciDelGas.crud.OrdineList;
+import org.domain.SeamAmiciDelGas.entity.Ordine;
+
+
+@Name("ordineBean")
+@Scope(ScopeType.SESSION)
+public class OrdineBean {
+
+	@Logger
+	private Log log;
+	
+	@In private Credentials credentials;
+	
+	@In(value="ordineList",create=true)
+	private OrdineList ordineList;
+	
+	private String tipoOrdine="1";
+	
+	private List<Ordine> ordini;
+
+	public String getTipoOrdine() {
+		return tipoOrdine;
+	}
+
+	public void setTipoOrdine(String tipoOrdine) {
+		log.info("****ho selezionato un ordine****");
+		log.info("*******"+tipoOrdine+"**********");
+		this.tipoOrdine = tipoOrdine;
+	}
+
+	public List<Ordine> getOrdini() {
+		log.info("*******"+credentials.getUsername()+"**********");
+		log.info("*******"+tipoOrdine+"**********");
+		if (tipoOrdine.equals("1"))
+			ordineList.setEjbql("select ordine from Ordine ordine where ordine.account.username='"+credentials.getUsername()+"' and ordine.cancellato='false'");
+		//selezioni gli ordini pendenti
+		if (tipoOrdine.equals("2"))
+			ordineList.setEjbql("select ordine from Ordine ordine where ordine.account.username='"+credentials.getUsername()+"' and ordine.cancellato='false' and ordine.pendente='true'");
+		//selezioni gli ordini conclusi
+		if (tipoOrdine.equals("3"))
+			ordineList.setEjbql("select ordine from Ordine ordine where ordine.account.username='"+credentials.getUsername()+"' and ordine.cancellato='false' and ordine.pendente='false'");
+		ordini = ordineList.getResultList();
+		return ordini;
+	}
+
+	public void setOrdini(List<Ordine> ordini) {
+		this.ordini = ordini;
+	}
+	
+}
