aff2e03b8814ad8140e32294c0432c8bbcc473ad
laserbnc <laserbnc@262607f0-e6ff-11dd-bce9-bf72c0239d3a>
2009-03-09 16:07:41 +0000

    FUNZIONA TUTTO

--- SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/processes/CancelTaskAction.java
+++ SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/processes/CancelTaskAction.java
@@ -1,28 +1,33 @@
 package org.domain.SeamAmiciDelGas.processes;
-
-
-
-
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
+import java.util.Collection;
+import java.util.Iterator;
+import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
-@Name("cancelAction")
-@Scope(ScopeType.BUSINESS_PROCESS)
-public class CancelTaskAction {
-	
-	 /**
-	 */
-	private static final long serialVersionUID = 6157822508821288152L;
-	/**
-	 * 
-	 */
+import org.jbpm.taskmgmt.exe.TaskInstance;
 
-	public void execute() throws Exception {
+public class CancelTaskAction implements ActionHandler{
+	
+	private static final long serialVersionUID = 6157822508821288152L;
+
+	public void execute(ExecutionContext executionContext) throws Exception 
+	{
+		
 		    
-		  ExecutionContext executionContext= ExecutionContext.currentExecutionContext();
-		  System.out.println("Excetute TTTTTTTTTTTTTTTTTTTTTTTTTTT");
-		  executionContext.getTaskInstance().cancel();
-	  }
+		System.out.println("Excetute TTTTTTTTTTTTTTTTTTTTTTTTTTT");
+		TaskInstance  task = executionContext.getTaskInstance();
+		Collection<TaskInstance> col  = executionContext.getTaskMgmtInstance().getTaskInstances();
+		Iterator<TaskInstance> it = col.iterator();
+		while(it.hasNext())
+		{
+			task = (TaskInstance) it.next();
+			System.out.println("IL TASKKKKKKKKKK "+task.getName());
+			if(task.getName().equals("partecipa_alla_visita") && task.isOpen())
+			{
+				System.out.println("Task ID: "+task.getId());
+				task.setSignalling(false);
+				task.cancel();
+			}
+		}
+	}
 
 }
--- SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/processes/TimerHandler.java
+++ SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/processes/TimerHandler.java
@@ -1,77 +1,51 @@
 package org.domain.SeamAmiciDelGas.processes;
 
-import java.io.Serializable;
-
 import java.util.Date;
-import java.util.GregorianCalendar;
-
-
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
-
-import org.jbpm.graph.def.Action;
-import org.jbpm.graph.def.Node;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.jbpm.job.Timer;
-import org.jbpm.scheduler.SchedulerService;
 
-import org.jbpm.svc.Services;
-import org.jbpm.taskmgmt.exe.TaskInstance;
-
-@Name("timerHandler")
-@Scope(ScopeType.BUSINESS_PROCESS)
-public class TimerHandler implements Serializable {
+public class TimerHandler implements ActionHandler {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3037017664789033945L;
+	
+	  String timerName;
 	  
-	 @In(value="dataProposta", scope=ScopeType.BUSINESS_PROCESS) 
-	 private Date dataProposta;
-
-	  public void execute() {
+	  private static final Log log = LogFactory.getLog(TimerHandler.class);
+	
+	public void execute(ExecutionContext executionContext ) {
 	    
-		  ExecutionContext executionContext = ExecutionContext.currentExecutionContext();
-		  System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		  long data = dataProposta.getTime() - System.currentTimeMillis();
-		  Date dueDate = new Date();
-		  if(data < 0)
-			  dueDate.setTime(0);
-		  else 
-			  dueDate.setTime(data);
 		 
-		  System.out.println("TEMPOOOOOOOOOOOOOOOO: "+dueDate.toString());
+		  System.out.println("AAAAAAAAAAAAAAAncora");
 		
-		 TaskInstance t = executionContext.getTaskInstance();
-		 System.out.println("Prelevata l'istanza "+ t.getId()+"del task"+ t.getTask().getName());
-		 Timer timer = new Timer(t.getToken());
-		 timer.setName("TimerPartecipa");
-		 timer.setTransitionName("time-out");
-		 GregorianCalendar gc= new GregorianCalendar();
-		 gc.roll(GregorianCalendar.MINUTE, 1);
-			 timer.setDueDate(gc.getTime());
-			
-		 timer.setTaskInstance(t);
-		 Node currentNode=t.getTask().getTaskNode();
-		 timer.setGraphElement(currentNode);
-		 currentNode.fireEvent("timer-create", executionContext);
-		 CancelTaskAction act = new CancelTaskAction();
-		 Action azione = new Action();
-		 azione.setName("Cancella Task");
-		 azione.setActionExpression("#{cancelAction.execute}");
-		 timer.setAction(azione);
-		 
-		 executionContext.setTimer(timer);
-		 
-		 SchedulerService s = (SchedulerService) executionContext.getJbpmContext().getServices().getService(Services.SERVICENAME_SCHEDULER);
-		 s.createTimer(timer);
-		// t.cancel();
-		//  executionContext.getTask().
-		 System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		  try {
+
+			  
+	          Timer timer = executionContext.getTimer();
+	          
+	          if (timer != null && timerName.equals(timer.getName())) {
+	               Date dataQuestionario = (Date) executionContext.getVariable("dataQuestionario");
+	               System.out.println("DataQuestonario: "+dataQuestionario);
+	               timer.setDueDate(dataQuestionario);
+	          } else {
+	               log.debug("Doesn't match: " + timer);
+	          }
+	     } catch (Exception ex) {
+	          ex.printStackTrace();
+	     }
 	 
 		 
 	  }
+
+	public String getTimerName() {
+		return timerName;
+	}
+
+	public void setTimerName(String timerName) {
+		this.timerName = timerName;
+	}
 	  
 }
 
