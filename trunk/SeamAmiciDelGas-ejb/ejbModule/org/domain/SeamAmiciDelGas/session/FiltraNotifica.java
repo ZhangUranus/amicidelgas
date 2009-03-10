743568cb5faa5011b8b9e24cbd5611479dd1d976
clelio83 <clelio83@262607f0-e6ff-11dd-bce9-bf72c0239d3a>
2009-03-10 14:40:25 +0000

    notifiche customer driver

--- trunk/SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/session/FiltraNotifica.java
+++ trunk/SeamAmiciDelGas-ejb/ejbModule/org/domain/SeamAmiciDelGas/session/FiltraNotifica.java
@@ -1,5 +1,8 @@
 package org.domain.SeamAmiciDelGas.session;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
@@ -14,41 +17,85 @@ import org.jbpm.taskmgmt.exe.TaskInstance;
 public class FiltraNotifica {
 
 	@In(value="pooledTaskInstanceList", create=true)
-	private PooledTaskInstanceList pooledTaskInstanceList;
+	private List<TaskInstance> pooledTaskInstanceList;
 	
 	@In(value="taskInstanceList", create=true)
-	private TaskInstanceList taskInstanceList;
+	private List<TaskInstance> taskInstanceList;
 	
 	@In private Credentials credentials;
 	
 	public int numberForSingleForCustomer() {
 		int count = 0;
-		for (TaskInstance ti: taskInstanceList.getTaskInstanceList()) {
+		for (TaskInstance ti: taskInstanceList) {
 			if (ti.getVariable("notifyMessage")!=null)
 				count++;
 		}
 		return count;
+	}
+	
+	public List<TaskInstance> taskInstanceSingleListForCustomer() {
+		List<TaskInstance> tasks = new ArrayList<TaskInstance>();
+		for (TaskInstance ti: taskInstanceList) {
+			if (ti.getVariable("notifyMessage")!=null)
+				tasks.add(ti);
+		}
+		return tasks;
 	}
 	
 	public int numberForGroupForCustomer() {
 		int count = 0;
-		for (TaskInstance ti: pooledTaskInstanceList.getPooledTaskInstanceList()) {
+		for (TaskInstance ti: pooledTaskInstanceList) {
 			if (ti.getVariable("notifyMessage")!=null)
 				count++;
 		}
 		return count;
 	}
 	
-	/*
-	public int numberForSingleForDriver() {
-		
+	public List<TaskInstance> taskInstanceGroupListForCustomer() {
+		List<TaskInstance> tasks = new ArrayList<TaskInstance>();
+		for (TaskInstance ti: pooledTaskInstanceList) {
+			if (ti.getVariable("notifyMessage")!=null)
+				tasks.add(ti);
+		}
+		return tasks;
 	}
-	*/
 	
-	/*
-	public int numberForGroupForDriver() {
-		
+	
+	public int numberForSingleForDriver() {
+		int count = 0;
+		for (TaskInstance ti: taskInstanceList) {
+			if (ti.getVariable("notificaDriverContadino")!=null)
+				count++;
+		}
+		return count;
 	}
-	*/
+	
+	public List<TaskInstance> taskInstanceSingleListForDriver() {
+		List<TaskInstance> tasks = new ArrayList<TaskInstance>();
+		for (TaskInstance ti: taskInstanceList) {
+			if (ti.getVariable("notificaDriverContadino")!=null)
+				tasks.add(ti);
+		}
+		return tasks;
+	}
+	
+	public int numberForGroupForDriver() {
+		int count = 0;
+		for (TaskInstance ti: pooledTaskInstanceList) {
+			if (ti.getVariable("notificaDriverContadino")!=null)
+				count++;
+		}
+		return count;
+	}
+	
+	public List<TaskInstance> taskInstanceGroupListForDriver() {
+		List<TaskInstance> tasks = new ArrayList<TaskInstance>();
+		for (TaskInstance ti: pooledTaskInstanceList) {
+			if (ti.getVariable("notificaDriverContadino")!=null)
+				tasks.add(ti);
+		}
+		return tasks;
+	}
+	
 	
 }
